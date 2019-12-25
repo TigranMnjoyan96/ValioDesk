726850c14f7cebc12b933d62b54fd380813b394d
TigranMnjoyan96 <mnjoyan1996@gmail.com>
2019-12-26 00:16:15 +0400

    Updates

---
+++ precache-manifest.91714399cb317ca0414b23684dd7cb75.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "28bffef7da8dd04b3e0a4ac9134137d8",
+    "url": "/ValioDesk/index.html"
+  },
+  {
+    "revision": "35552f41e31a50f1a050",
+    "url": "/ValioDesk/static/css/main.13d4f757.chunk.css"
+  },
+  {
+    "revision": "2a949a690a297cd1ee78",
+    "url": "/ValioDesk/static/js/2.0117bf01.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/ValioDesk/static/js/2.0117bf01.chunk.js.LICENSE"
+  },
+  {
+    "revision": "35552f41e31a50f1a050",
+    "url": "/ValioDesk/static/js/main.38e0ef08.chunk.js"
+  },
+  {
+    "revision": "07691be12bde9843f0d7",
+    "url": "/ValioDesk/static/js/runtime-main.1dcfdba1.js"
+  },
+  {
+    "revision": "8c89e8d51b62251f4a6e99982c0d6418",
+    "url": "/ValioDesk/static/media/arajatarner.8c89e8d5.png"
+  },
+  {
+    "revision": "7f4ab7017012da29857bf081655307cd",
+    "url": "/ValioDesk/static/media/finish.7f4ab701.png"
+  },
+  {
+    "revision": "8344eb4b687e70590c5d42f376caabec",
+    "url": "/ValioDesk/static/media/questEight.8344eb4b.png"
+  },
+  {
+    "revision": "66f181bc634ad2aed45325af5090c256",
+    "url": "/ValioDesk/static/media/questFive.66f181bc.png"
+  },
+  {
+    "revision": "0f67badc86167c2804e36e2efcf48dbf",
+    "url": "/ValioDesk/static/media/questFour.0f67badc.png"
+  },
+  {
+    "revision": "faac3e45869c1592a757b832f9446f9c",
+    "url": "/ValioDesk/static/media/questNine.faac3e45.png"
+  },
+  {
+    "revision": "67dfd19cdb4dd9c2bd156cd0cb8c4bb8",
+    "url": "/ValioDesk/static/media/questOne.67dfd19c.png"
+  },
+  {
+    "revision": "704c27ca8f75408715480d9df606d0ea",
+    "url": "/ValioDesk/static/media/questSeven.704c27ca.png"
+  },
+  {
+    "revision": "fce980dcfed3287b57fc499223fd7b89",
+    "url": "/ValioDesk/static/media/questSix.fce980dc.png"
+  },
+  {
+    "revision": "4a9be714fe7b222f3c6c2ab2fae6887c",
+    "url": "/ValioDesk/static/media/questTen.4a9be714.png"
+  },
+  {
+    "revision": "f2b14ca1d1e3fada30220a2ed5927eb2",
+    "url": "/ValioDesk/static/media/questTwo.f2b14ca1.png"
+  },
+  {
+    "revision": "a885adc6aeaad7cbe665c067be442773",
+    "url": "/ValioDesk/static/media/valio 1.a885adc6.png"
+  },
+  {
+    "revision": "7a24f1fdd954e422de898f72f2beb6f4",
+    "url": "/ValioDesk/static/media/valio 2 (2).7a24f1fd.png"
+  },
+  {
+    "revision": "b50dde52c7aae8f3dbe018fa1eec9bb0",
+    "url": "/ValioDesk/static/media/valioMobile2.b50dde52.png"
+  },
+  {
+    "revision": "3988933cfd81eaaf6bab5f7b24c30e50",
+    "url": "/ValioDesk/static/media/valioMobileStart.3988933c.png"
+  }
+]);
